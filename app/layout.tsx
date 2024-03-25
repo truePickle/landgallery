93de244dfe75a3a764f38e4948dd1160a7df76c1
Noah Brockschmidt <noah.brockschmidt2003@gmail.com>
2024-03-25 17:15:28 +0100

    fixed hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className='dark'>
-      <Providers>
-      <body className={inter.className}>{children}</body>
-      </Providers>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
