96986ba3bb18c0fcd770b4d0095074efa0c774b2
noah <noah.brockschmidt@gmail.com>
2024-04-09 13:44:16 +0200

    deleted everything

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@nextui-org/react";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello World</Button>
-    </main>
-  );
-}
